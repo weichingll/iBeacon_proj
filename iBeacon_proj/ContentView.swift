dc8620f7108344df1d709c82a892674fc8e7f9a3
leo <s100411@gm.pu.edu.tw>
2024-05-05 21:48:50 +0800

    Initial Commit

---
+++ iBeacon_proj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iBeacon_proj
+//
+//  Created by 呂長霖 on 2024/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iBeacon_proj/iBeacon_projApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBeacon_projApp.swift
+//  iBeacon_proj
+//
+//  Created by 呂長霖 on 2024/5/5.
+//
+
+import SwiftUI
+
+@main
+struct iBeacon_projApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
